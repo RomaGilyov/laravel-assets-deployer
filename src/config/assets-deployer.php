11d67b104a9b87c67bd7abf7287323dd96a4ac62
RomaGilyov <rgilyov@gmail.com>
2018-07-11 16:08:45 +0300

    init

---
+++ src/config/assets-deployer.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Files for deployment
+    |--------------------------------------------------------------------------
+    |
+    | List of asset files path names
+    |
+    | If you need to specify buildDirectory/manifestDirectory you will need to
+    | add it as a `pathname` => `buildDirectory/manifestDirectory`
+    |
+    */
+
+    'deploy' => [
+
+    ],
+];
