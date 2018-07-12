2b892620a11c3f0fa91b02f8fdf72ecb7aacefb8
RomaGilyov <rgilyov@gmail.com>
2018-07-12 16:58:47 +0300

    assets deployer

---
+++ src/Providers/AssetsDeployerServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace RGilyov\AssetsDeployer\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class AssetsDeployerServiceProvider
+ * @package RGilyov\AssetsDeployer\Providers
+ */
+class AssetsDeployerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/assets-deployer.php' => config_path('assets-deployer.php')
+        ], 'config');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/assets-deployer.php', 'assets-deployer');
+
+        if (function_exists('assets_deployer_get')) {
+            require_once __DIR__."../helpers.php";
+        }
+
+        if (method_exists($this, 'commands')) {
+            $this->commands([]);
+        }
+    }
+}
