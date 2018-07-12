52a27d1b34925bd215b9b25662490a40b0a387ae
RomaGilyov <rgilyov@gmail.com>
2018-07-12 18:02:27 +0300

    assets deployer

--- src/Providers/AssetsDeployerServiceProvider.php
+++ src/Providers/AssetsDeployerServiceProvider.php
@@ -19,7 +19,7 @@ class AssetsDeployerServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config/assets-deployer.php' => config_path('assets-deployer.php')
+            __DIR__.'../config/assets-deployer.php' => config_path('assets-deployer.php')
         ], 'config');
     }
 
@@ -30,7 +30,7 @@ class AssetsDeployerServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/assets-deployer.php', 'assets-deployer');
+        $this->mergeConfigFrom(__DIR__ . '../config/assets-deployer.php', 'assets-deployer');
 
         require_once __DIR__."../helpers.php";
 
